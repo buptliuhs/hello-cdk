ddb6464496c8bc0fd524c1d49f52f2b5ac9daa13
Tony Liu <buptliuhs@gmail.com>
2020-03-03 22:33:11 +1300

    First attempt to create a stack.

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
 
 export class HelloCdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        // The code that defines your stack goes here
+        new s3.Bucket(this, "MyFirstBucket", {
+            bucketName: cdk.PhysicalName.GENERATE_IF_NEEDED,
+            versioned: true
+        });
+    }
 }
