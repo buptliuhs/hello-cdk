bd77d695d0f0fd8eac68a5592594849ef2f60294
Tony Liu <buptliuhs@gmail.com>
2020-03-10 20:35:42 +1300

    Update test.

--- test/hello-cdk.test.ts
+++ test/hello-cdk.test.ts
@@ -1,12 +1,12 @@
-import {MatchStyle, expect as expectCDK, matchTemplate} from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import HelloCdk = require('../lib/hello-cdk-stack');
+import {MatchStyle, expect as expectCDK, matchTemplate} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import HelloCdk = require("../lib/hello-cdk-stack");
 
-test('Empty Stack', () => {
+test("Empty Stack", () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new HelloCdk.HelloCdkStack(app, 'HelloCdkStack');
+  const stack = new HelloCdk.HelloCdkStack(app, "HelloCdkStack");
   // THEN
-  const stackJson = require('./data/hello-cdk');
-  expectCDK(stack).to(matchTemplate(stackJson, MatchStyle.EXACT));
+  const stackJson = require("./data/hello-cdk");
+  expectCDK(stack).to(matchTemplate(stackJson, MatchStyle.SUPERSET));
 });
